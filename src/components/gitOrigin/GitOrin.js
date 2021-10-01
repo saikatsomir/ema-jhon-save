a142854528c22f42077ff511d9de11db42d55aff
saikatsomir <saikatsomir@gmail.com>
2021-10-01 12:27:32 +0600

    second commit

---
+++ src/components/gitOrigin/GitOrin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GitOrin = () => {
+    return (
+        <div>
+            <h2>Hello guys how are you all?</h2>
+        </div>
+    );
+};
+
+export default GitOrin;
